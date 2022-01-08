5f4f4911683a53637a1990494cc859b882db98a0
Jared Rowe <jared.w.rowe@gmail.com>
2022-01-08 06:17:37 -0800

    Added rod details and attachment UI
    Right side of the rod inventory tab has larger image, title, description, and 3 attachments (bait, line, and hook) with UI set up for selecting which attachments. No code implemented yet.

---
+++ Assets/Scripts/UI/Inventory/RodAttachmentButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RodAttachmentButton : MonoBehaviour
+{
+    [SerializeField] private ScrollRect scrollView;
+
+    public void ToggleScrollView()
+    {
+        scrollView.gameObject.SetActive(!scrollView.gameObject.activeSelf);
+    }
+}
