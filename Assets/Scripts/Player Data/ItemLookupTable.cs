ac6f0d8ea884f353c6f271bde1beb9040ca9c8f3
Jared Rowe <jared.w.rowe@gmail.com>
2022-01-11 07:51:25 -0800

    Began lookup table script
    Started writing a script to use as a lookup table for converting player items from game objects into data for storage and vise-versa

---
+++ Assets/Scripts/Player Data/ItemLookupTable.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemLookupTable : MonoBehaviour
+{
+    public enum ItemType { Fishable, Rod, Gear, Bait }
+
+    [Header("Fishable Items")]
+    public List<string> fishableNames;
+    public List<GameObject> fishablePrefabs;
+
+    [Header("Fishing Rods")]
+    public List<string> rodNames;
+    public List<GameObject> rodPrefabs;
+
+    [Header("Gear")]
+    public List<string> gearNames;
+    public List<GameObject> gearPrefabs;
+
+    [Header("Bait")]
+    public List<string> baitNames;
+    public List<GameObject> baitPrefabs;
+
+    FishableData FishableToData(FishableItem fish)
+    {
+        return new FishableData(fish.GetName(), fish.GetWeight(), fish.GetLength());
+    }
+}
+
+public class FishableData
+{
+    string name;
+    float weight;
+    float length;
+
+    public FishableData(string _name, float _weight, float _length)
+    {
+        name = _name;
+        weight = _weight;
+        length = _length;
+    }
+}
