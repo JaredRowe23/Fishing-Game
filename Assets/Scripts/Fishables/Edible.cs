0d7e83af012200afc640012d1f2e29282bb44850
Jared Rowe <jared.w.rowe@gmail.com>
2023-02-12 02:39:24 -0800

    Anglerfish
    Added a new Anglerfish prefab. This one behaves the same as Salmon and Carp, however it has a child object that masks the camera's darkening as you go deeper, creating a basic lighting effect surrounding them.

--- Assets/Scripts/Fishables/Edible.cs
+++ Assets/Scripts/Fishables/Edible.cs
@@ -8,7 +8,7 @@ namespace Fishing.Fishables
     public class Edible : MonoBehaviour
     {
         public float baseFoodAmount;
-        public enum FoodTypes { Hook, Salmon, TinCan, EarthWorm, Carp, Seaweed, Boot, Driftwood, Minnow, WaterLilyFruit, Fish10, Fish11 };
+        public enum FoodTypes { Hook, Salmon, TinCan, EarthWorm, Carp, Seaweed, Boot, Driftwood, Minnow, WaterLilyFruit, Anglerfish, Fish11 };
         [SerializeField] private FoodTypes foodType;
 
         private void Start() => FoodSearchManager.instance.AddFood(this);
