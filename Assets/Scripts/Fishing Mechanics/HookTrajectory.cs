6eadc655bae48523558ba79d7be642d939f4dae5
Jared Rowe <jared.w.rowe@gmail.com>
2024-03-03 02:32:04 -0800

    Min/Max Trajectories
    Added UI for the minimum and maximum power trajectories.
    
    These will display from the start of the casting process through to the cast, giving the player an idea of what angles provide which possibilities before they lock it in and begin choosing the power of the cast.
    
    Still need to adjust camera size to better view the furthest distances.

--- Assets/Scripts/Fishing Mechanics/HookTrajectory.cs
+++ Assets/Scripts/Fishing Mechanics/HookTrajectory.cs
@@ -6,56 +6,79 @@ namespace Fishing.FishingMechanics
 {
     public class HookTrajectory : MonoBehaviour
     {
-        private LineRenderer lineRenderer;
         private PowerAndAngle powerAngle;
 
         [SerializeField] private float trajectoryStep = 0.1f;
         [SerializeField] private int trajectoryMaxSteps = 20;
 
+        [SerializeField] private LineRenderer trajectoryLineRenderer;
+        [SerializeField] private LineRenderer minTrajectoryLineRenderer;
+        [SerializeField] private LineRenderer maxTrajectoryLineRenderer;
+
+        [SerializeField] private Material trajectoryMat;
+        [SerializeField] private Material minTrajectoryMat;
+        [SerializeField] private Material maxTrajectoryMat;
+
         private List<Vector2> trajectoryPoints;
+        private List<Vector2> minimumTrajectoryPoints;
+        private List<Vector2> maximumTrajectoryPoints;
 
         private RodBehaviour equippedRod;
 
         private void Awake()
         {
             powerAngle = PowerAndAngle.instance;
-        }
 
-        private void Start()
-        {
-            lineRenderer = GetComponent<LineRenderer>();
+            trajectoryLineRenderer.material = trajectoryMat;
+            minTrajectoryLineRenderer.material = minTrajectoryMat;
+            maxTrajectoryLineRenderer.material = maxTrajectoryMat;
         }
 
         private void Update()
         {
-            if (!powerAngle.GetCharging())
+            if (!powerAngle.GetCharging() && !powerAngle.GetAngling())
             {
-                lineRenderer.enabled = false;
+                trajectoryLineRenderer.enabled = false;
+                minTrajectoryLineRenderer.enabled = false;
+                maxTrajectoryLineRenderer.enabled = false;
                 return;
             }
 
-            lineRenderer.enabled = true;
-
             equippedRod = RodManager.instance.equippedRod;
 
-            trajectoryPoints = SimulateTrajectory();
-            lineRenderer.positionCount = trajectoryPoints.Count;
-            for (int i = 0; i < trajectoryPoints.Count; i++)
-            {
-                lineRenderer.SetPosition(i, trajectoryPoints[i]);
-            }
+            minimumTrajectoryPoints = GetTrajectoryPoints(equippedRod.scriptable.minCastStrength);
+            maximumTrajectoryPoints = GetTrajectoryPoints(equippedRod.scriptable.maxCastStrength);
 
-            float width = lineRenderer.startWidth;
-            lineRenderer.material.mainTextureScale = new Vector2(1f / width, 1.0f);
+            PlotTrajectory(minimumTrajectoryPoints, minTrajectoryLineRenderer);
+            PlotTrajectory(maximumTrajectoryPoints, maxTrajectoryLineRenderer);
+
+            if (!powerAngle.GetCharging()) return;
+
+            trajectoryPoints = GetTrajectoryPoints(Mathf.Lerp(equippedRod.scriptable.minCastStrength, equippedRod.scriptable.maxCastStrength, powerAngle.GetCharge()));
+            PlotTrajectory(trajectoryPoints, trajectoryLineRenderer);
         }
 
-        private List<Vector2> SimulateTrajectory()
+        private void PlotTrajectory(List<Vector2> _points, LineRenderer _lineRenderer)
+        {
+            _lineRenderer.enabled = true;
+
+            _lineRenderer.positionCount = _points.Count;
+
+            for (int i = 0; i < _points.Count; i++)
+            {
+                _lineRenderer.SetPosition(i, _points[i]);
+            }
+
+            float width = _lineRenderer.startWidth;
+            _lineRenderer.material.mainTextureScale = new Vector2(1f / width, 1.0f);
+        }
+
+        private List<Vector2> GetTrajectoryPoints(float _force)
         {
             trajectoryPoints = new List<Vector2>();
 
             Vector2 _launchPos = transform.parent.position;
             float _mass = equippedRod.GetHook().GetComponent<Rigidbody2D>().mass;
-            float _force = Mathf.Lerp(equippedRod.scriptable.minCastStrength, equippedRod.scriptable.maxCastStrength, powerAngle.GetCharge());
             float _angle = 90f - powerAngle.GetAngle();
             Vector2 _directionVector = new Vector2(Mathf.Sin(_angle * Mathf.Deg2Rad), Mathf.Cos(_angle * Mathf.Deg2Rad));
 
