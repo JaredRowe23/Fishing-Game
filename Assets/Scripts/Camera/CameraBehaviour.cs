05d96fff96e639f1d923101499586d86461a720c
Jared Rowe <jared.w.rowe@gmail.com>
2024-11-30 08:01:41 -0800

    v0.16.1 - CameraBehaviour script Tooltips
    Added tooltips for variables exposed to the inspector.

--- Assets/Scripts/Camera/CameraBehaviour.cs
+++ Assets/Scripts/Camera/CameraBehaviour.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 namespace Fishing.PlayerCamera {
     public class CameraBehaviour : MonoBehaviour {
         [Header("Position And Tracking")]
-        [SerializeField] private Vector2 _defaultPosition;
+        [SerializeField, Tooltip("Position camera rests at when player is idle. Set on start.")] private Vector2 _defaultPosition;
 
-        [SerializeField, Range(0f, 1.0f)] private float _minFollowSpeed = 0.1f;
-        [SerializeField, Range(0f, 1.0f)] private float _maxFollowSpeed = 1f;
-        [SerializeField, Range(0f, 1.0f)] private float _followThreshold = 0.1f;
+        [SerializeField, Range(0f, 1.0f), Tooltip("Minimum speed for lerping camera to target position.")] private float _minFollowSpeed = 0.001f;
+        [SerializeField, Range(0f, 1.0f), Tooltip("Maximum speed for lerping camera to target position.")] private float _maxFollowSpeed = 0.05f;
+        [SerializeField, Range(0f, 1.0f), Tooltip("Distance camera will stop moving to follow target position.")] private float _followThreshold = 0.1f;
 
         [Header("Zoom")]
-        [SerializeField, Range(0f, 1.0f)] private float _zoomSpeed = 0.5f;
-        [SerializeField] private float _zoomThreshold = 0.1f;
+        [SerializeField, Range(0f, 1.0f), Tooltip("Speed camera will zoom in at (not scaled with deltaTime).")] private float _zoomSpeed = 0.5f;
+        [SerializeField, Tooltip("Value difference camera will stop adjusting zoom to match target zoom.")] private float _zoomThreshold = 0.01f;
 
-        [SerializeField, Min(0)] private float _zoomMagnitude = 1;
-        [SerializeField, Min(0)] private float _minPlayerZoom = 1;
-        [SerializeField, Min(0)] private float _maxPlayerZoom = 20;
+        [SerializeField, Min(0), Tooltip("Amount Camera.orthographicSize is adjusted for each zoom action.")] private float _zoomMagnitude = 1;
+        [SerializeField, Min(0), Tooltip("Minimum value for Camera.orthographicSize when zooming.")] private float _minPlayerZoom = 1;
+        [SerializeField, Min(0), Tooltip("Maximum value for Camera.orthographicSize when zooming.")] private float _maxPlayerZoom = 20;
 
         private bool _lockZoom = false;
         private bool _lockPosition = false;
