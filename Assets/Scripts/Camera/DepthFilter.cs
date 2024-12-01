e822a5ba8c3ba558987015fb1194b401c8d4908b
Jared Rowe <jared.w.rowe@gmail.com>
2024-12-01 15:51:09 -0800

    v0.16.1e - DepthFilter script update
    Added OnValidate to manage _minDepth not going above _maxDepth. Added Range attribute to _maxAlpha. Added Colors for drawing depth gizmos.

--- Assets/Scripts/Camera/DepthFilter.cs
+++ Assets/Scripts/Camera/DepthFilter.cs
@@ -5,26 +5,36 @@ using Fishing.Util;
 
 namespace Fishing.PlayerCamera {
     public class DepthFilter : MonoBehaviour {
-        [SerializeField] private float _maxAlpha;
+        [SerializeField, Range(0, 1f)] private float _maxAlpha;
         [SerializeField] private float _minDepth;
         [SerializeField] private float _maxDepth;
         private SpriteRenderer _sprite;
+
+        [Header("Gizmos")]
+        [SerializeField] private Color _minDepthGizmoColor;
+        [SerializeField] private Color _maxDepthGizmoColor;
+
+        private void OnValidate() {
+            if (_minDepth > _maxDepth) {
+                _minDepth = _maxDepth;
+            }
+        }
 
         private void Start() {
             _sprite = GetComponent<SpriteRenderer>();
         }
 
         void Update() {
-            if (-transform.position.y < _minDepth) { 
+            if (-transform.position.y < _minDepth || _sprite.color.a == 0f) { 
                 return; 
             }
             _sprite.color = Utilities.SetTransparency(_sprite.color, Mathf.InverseLerp(_minDepth, _maxDepth, -transform.position.y) * _maxAlpha);
         }
 
         private void OnDrawGizmosSelected() {
-            Gizmos.color = Color.green;
+            Gizmos.color = _minDepthGizmoColor;
             Gizmos.DrawLine(new Vector3(-1000, -_minDepth, 0), new Vector3(1000, -_minDepth, 0));
-            Gizmos.color = Color.red;
+            Gizmos.color = _maxDepthGizmoColor;
             Gizmos.DrawLine(new Vector3(-1000, -_maxDepth, 0), new Vector3(1000, -_maxDepth, 0));
         }
     }
